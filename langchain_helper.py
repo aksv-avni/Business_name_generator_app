40f6aaa3371c5ea82433dc12ee46d82dd3b525d9
Avni Singh <71398237+aksv-avni@users.noreply.github.com>
2024-04-14 03:14:08 +0530

    Add files via upload

---
+++ langchain_helper.py
@@ -0,0 +1,49 @@
+from langchain_cohere import ChatCohere
+from langchain_core.messages import AIMessage
+from langchain.prompts.chat import ChatPromptTemplate
+
+
+
+chat_model = ChatCohere(cohere_api_key="7vuRcifreUcrTmd7BhawUHKDpRgTFhKx89mRI51J", temperature = 0.6 )
+
+# os.environ["SERPAPI_API_KEY"] = "d6315857583bc9a0810143011f8a996a1b3f83324560162667cf7996ff266273"
+
+def gen_business_name(domain):
+    
+    template = "You are a helpful assistant that generates a Business name for a given product."
+    human_template = "give a company name for products of type : {product}"
+
+    chat_prompt = ChatPromptTemplate.from_messages([
+        ("system", template),
+        ("human", human_template),
+    ])
+
+
+    chain = chat_prompt | chat_model
+    
+    name_output = chain.invoke({"product" : domain}).content
+    
+    template_command = "You are a helpful assistant that generates all the latest trends for a given Business domain."
+    human_command = "give the latest trends for business domain : {domain}"
+    
+    trend_prompt = ChatPromptTemplate.from_messages([
+        ("system",template_command),
+        ("human", human_command)
+        ])
+    
+    trend_chain = trend_prompt | chat_model
+    
+    trend_output = trend_chain.invoke({"domain" : domain}).content
+    
+    output = (name_output, trend_output)
+    
+    return output
+
+if __name__ == "__main__":
+    print(gen_business_name("Technology"))
+    
+ #   tool = GoogleTrendsQueryRun(api_wrapper=GoogleTrendsAPIWrapper())
+ #   tool.run(domain)
+
+
+
---
+++ main.py
@@ -0,0 +1,59 @@
+
+import streamlit as st
+import langchain_helper
+
+st.title("NameSphere : Trend Explorer & Business Naming Guru")
+st.header("**Generate your business name and stay updated on the latest trends within your industry.**")
+
+domain = st.sidebar.selectbox("Choose your Domain", ("Technology","Apparels","Healthcare", "Finance","Retail"
+                                            ,"Fashion and Beauty","Hospitality", "Education",
+                                            "Sports"))
+
+if domain :
+    response = langchain_helper.gen_business_name(domain)
+    if domain == "Technology":
+        st.image("https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0])
+        st.write(response[1])
+    if domain == "Apparels":
+        st.image("https://images.unsplash.com/photo-1523381294911-8d3cead13475?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Healthcare":
+        st.image("https://plus.unsplash.com/premium_photo-1674499074438-8f611a3569f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Finance":
+        st.image("https://plus.unsplash.com/premium_photo-1681469490209-c2f9f8f5c0a2?q=80&w=1783&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Retail":
+        st.image("https://plus.unsplash.com/premium_photo-1664202219714-7451573a4fa7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Fashion and Beauty":
+        st.image("https://images.unsplash.com/photo-1621797064712-2ed3a36fc009?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Hospitality":
+        st.image("https://images.unsplash.com/photo-1544986581-efac024faf62?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Education":
+        st.image("https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+    if domain == "Sports":
+        st.image("https://images.unsplash.com/photo-1517649763962-0c623066013b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                 width = 300)
+        st.write(response[0]) 
+        st.write(response[1])
+        
