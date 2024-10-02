d0df2ea79dfd5045ef70376a89134ac71f759f21
Avni Singh <71398237+aksv-avni@users.noreply.github.com>
2024-10-02 18:24:36 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -4,7 +4,7 @@ from langchain.prompts.chat import ChatPromptTemplate
 
 
 
-chat_model = ChatCohere(cohere_api_key="X1bXjIYCd9N7Bf0onUd6Qao0UozOu5gNBj6rE58u", model = "command-r+", max_tokens = 100, temperature = 0.3 )
+chat_model = ChatCohere(cohere_api_key="X1bXjIYCd9N7Bf0onUd6Qao0UozOu5gNBj6rE58u", model = "command-r", max_tokens = 100, temperature = 0.3 )
 
 def gen_business_name(domain):
     
